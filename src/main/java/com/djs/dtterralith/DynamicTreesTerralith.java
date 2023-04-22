e099085d41f6b7d50554820483641bb941cb5f9b
Dani <djsimmonds@gmail.com>
2023-04-22 18:46:35 -0400

    added config to load built-in datapacks to address specific fixes

--- src/main/java/com/djs/dtterralith/DynamicTreesTerralith.java
+++ src/main/java/com/djs/dtterralith/DynamicTreesTerralith.java
@@ -1,17 +1,30 @@
 package com.djs.dtterralith;
 
+import java.io.IOException;
+
 import com.djs.dtterralith.registry.DTTRegistries;
 import com.djs.dtterralith.registry.RegisterTerralithBiomes;
 import com.ferreusveritas.dynamictrees.api.GatherDataHelper;
 import com.ferreusveritas.dynamictrees.api.registry.RegistryHandler;
 import com.ferreusveritas.dynamictrees.block.leaves.LeavesProperties;
 import com.ferreusveritas.dynamictrees.tree.family.Family;
+
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.metadata.pack.PackMetadataSection;
+import net.minecraft.server.packs.repository.Pack;
+import net.minecraft.server.packs.repository.PackSource;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.AddPackFindersEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig.Type;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
+import net.minecraftforge.resource.PathResourcePack;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(DynamicTreesTerralith.MOD_ID)
@@ -21,8 +34,10 @@ public class DynamicTreesTerralith {
 
     public DynamicTreesTerralith() {
         final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		ModLoadingContext.get().registerConfig(Type.COMMON, DTTConfigs.GENERAL_SPEC, "dtterralithconfig.toml");		
 
         modEventBus.addListener(this::gatherData);
+		modEventBus.addListener(this::setupBuiltInDatapack);
         MinecraftForge.EVENT_BUS.register(this);
 
         RegistryHandler.setup(MOD_ID);
@@ -42,4 +57,62 @@ public class DynamicTreesTerralith {
         return new ResourceLocation(MOD_ID, name);
     }
 
+	private void setupBuiltInDatapack(AddPackFindersEvent event) {	
+
+		String MODID = "dtterralith";
+		
+		if (event.getPackType() == PackType.SERVER_DATA) {
+			if ((Boolean)DTTConfigs.OVERWORLD_JSON_FIX.get()) {
+				var resourcePath = ModList.get().getModFileById(MODID).getFile().findResource("resourcepacks/overworld_json_fix");
+
+	            try (var pack = new PathResourcePack(ModList.get().getModFileById(MODID).getFile().getFileName() + ":" + resourcePath, resourcePath)) {
+					var metadataSection = pack.getMetadataSection(PackMetadataSection.SERIALIZER);
+					if (metadataSection != null)
+					{
+					    event.addRepositorySource((packConsumer, packConstructor) ->
+					            packConsumer.accept(packConstructor.create(
+					            		"builtin/overworld_json_fix", new TextComponent("dtterralith fixes: overworld_json_fix"), false,
+					                    () -> pack, metadataSection, Pack.Position.TOP, PackSource.BUILT_IN, false)));
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			if ((Boolean)DTTConfigs.SKYLANDS_WINTER_FIX.get()) {
+				var resourcePath = ModList.get().getModFileById(MODID).getFile().findResource("resourcepacks/skylands_winter_fix");
+
+	            try (var pack = new PathResourcePack(ModList.get().getModFileById(MODID).getFile().getFileName() + ":" + resourcePath, resourcePath)) {
+					var metadataSection = pack.getMetadataSection(PackMetadataSection.SERIALIZER);
+					if (metadataSection != null)
+					{
+					    event.addRepositorySource((packConsumer, packConstructor) ->
+					            packConsumer.accept(packConstructor.create(
+					            		"builtin/skylands_winter_fix", new TextComponent("dtterralith fixes: skylands_winter_fix"), false,
+					                    () -> pack, metadataSection, Pack.Position.TOP, PackSource.BUILT_IN, false)));
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if ((Boolean)DTTConfigs.REPLACE_TREE_FEATURES_FIX.get()) {
+				var resourcePath = ModList.get().getModFileById(MODID).getFile().findResource("resourcepacks/replace_tree_features_fix");
+
+	            try (var pack = new PathResourcePack(ModList.get().getModFileById(MODID).getFile().getFileName() + ":" + resourcePath, resourcePath)) {
+					var metadataSection = pack.getMetadataSection(PackMetadataSection.SERIALIZER);
+					if (metadataSection != null)
+					{
+					    event.addRepositorySource((packConsumer, packConstructor) ->
+					            packConsumer.accept(packConstructor.create(
+					            		"builtin/replace_tree_features_fix", new TextComponent("dtterralith fixes: replace_tree_features_fix"), false,
+					                    () -> pack, metadataSection, Pack.Position.TOP, PackSource.BUILT_IN, false)));
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		
+		}    
+	}	
 }
